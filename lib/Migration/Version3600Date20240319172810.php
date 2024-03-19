d935c4e34ae55bcfc901817ef02cbb7692bfccf8
Anna Larch <anna@nextcloud.com>
2024-03-19 21:00:17 +0100

    fixup! fix: honour MDN requests

--- lib/Migration/Version3600Date20240319172810.php
+++ lib/Migration/Version3600Date20240319172810.php
@@ -1,27 +1,23 @@
 <?php
 
 declare(strict_types=1);
-
 /**
- * @copyright Copyright (c) 2024 FIXME Your name <your@email.com>
+ * @copyright 2024 Anna Larch <anna.larch@gmx.net>
  *
- * FIXME @author Your name <your@email.com>
+ * @author Anna Larch <anna.larch@gmx.net>
  *
- * @license GNU AGPL version 3 or any later version
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace OCA\Mail\Migration;
